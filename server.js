51927069a0509c30fdf35ebb9b5b85de6ef41548
Jimmie Jackson <jimmiejackson414@gmail.com>
2016-08-27 15:46:49 -0500

    heroku fix

--- server.js
+++ server.js
@@ -121,6 +121,6 @@ app.post('/articles/:id', function(req, res) {
 
 
 // listen on port 3000
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
 	console.log('App running on port 3000');
 });
